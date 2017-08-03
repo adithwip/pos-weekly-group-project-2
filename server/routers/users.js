fc9b46693f165c3ef2c66abffe1633fd290575bc
budhip <ryo.bunraku@yahoo.com>
2017-08-03 21:52:08 +0700

    fitur signin dan auth

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userCont = require('../controllers/userController');
+const userAuth = require('../helpers/verifyToken')
 
-router.get('/', userCont.getAllUsers);
-router.get('/:id', userCont.getOneUser);
-router.post('/', userCont.createNewUser);
-router.put('/:id', userCont.updateUser);
-router.delete('/:id', userCont.deleteUser);
+router.get('/', userAuth.verifyTokenForAdmin, userCont.getAllUsers);
+router.get('/:id', userAuth.verifyTokenForAdminKasir, userCont.getOneUser);
+router.post('/', userAuth.verifyTokenForAdmin, userCont.createNewUser);
+router.put('/:id', userAuth.verifyTokenForAdminKasir, userCont.updateUser);
+router.delete('/:id', userAuth.verifyTokenForAdmin, userCont.deleteUser);
 
 module.exports = router
